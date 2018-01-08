4389cdfff40eee72bf2a767986ec6b4087c96e0d
Quan Brew <t@ioover.net>
2018-01-08 21:41:44 +0800

    增加了FATE骰子以及CoC第七版人物属性生成功能
    Signed-off-by: Quan Brew <t@ioover.net>

--- bot.py
+++ bot.py
@@ -4,74 +4,238 @@ import secrets
 import logging
 import re
 
-from telegram import Bot, Update, ReplyKeyboardRemove, Chat
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
-from bot_token import BOT_TOKEN
+from telegram import Bot, Update
+from telegram.ext import Updater, CommandHandler
+from typing import List
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+with open('token.txt') as f:
+    BOT_TOKEN = f.read()
+
 DICE_TYPE_PATTERN = re.compile('^d(\d+)')
 DICE_ROLL_PATTERN = re.compile('^(\d+)d(\d+)$')
+FATE_ROLL_PATTERN = re.compile('^(\d+)d$')
+
 MAX_FACE = 1000
 MAX_NUM = 200
+WEB_HOOK = False
 
-def dice(face_num: int):
-    return secrets.randbelow(face_num) + 1
+
+class UnsupportedDice(Exception):
+    pass
+
+
+class Dice:
+    DEFAULT_NUM = 1
+
+    def __init__(self, face_num: int):
+        self.face = face_num
+
+    def roll(self) -> int:
+        return secrets.randbelow(self.face) + 1
+
+    def roll_n(self, dice_num) -> [int]:
+        return [self.roll() for _ in range(dice_num)]
+
+    def display(self, roll_result: [int]):
+        result = ", ".join(["{:2}".format(r) for r in roll_result])
+        num = len(roll_result)
+        if num == 1:
+            return "`1d{}` 🎲 `{}`".format(self.face, result)
+        else:
+            return "{}d{}` 🎲 `{}`\nsum: `{}` max: `{}` min: `{}`".format(
+                num, self.face, result, sum(roll_result), max(roll_result), min(roll_result))
+
+
+class FateDice(Dice):
+    DEFAULT_NUM = 4
+
+    def __init__(self):
+        super(FateDice, self).__init__(6)
+
+    def roll(self):
+        result = super(FateDice, self).roll()
+        if result > 4:
+            return 1
+        elif result < 3:
+            return -1
+        else:
+            return 0
+
+    def display(self, roll_result: [int]):
+        def to_str(x: int):
+            if x == 0:
+                return '⬜️️'
+            elif x > 0:
+                return '➕'
+            elif x < 0:
+                return '➖'
+        result = " ".join([to_str(r) for r in roll_result])
+        return "{}×FATE {} sum: `{}`".format(len(roll_result), result, sum(roll_result))
+
+
+def coc7stats(bot: Bot, update: Update, args: List[str]):
+    chat_id = update.message.chat_id
+
+    if len(args) != 1 or not args[0].isnumeric():
+        bot.send_message(chat_id,
+                         "召唤方式错误哦，只需要跟一个年龄参数，像这样 `/coc7stats 18` 。",
+                         parse_mode='Markdown')
+        return
+    age = int(args[0])
+    d6 = Dice(6)
+    d10 = Dice(10)
+    d100 = Dice(100)
+    str_ = sum(d6.roll_n(3)) * 5
+    con = sum(d6.roll_n(3)) * 5
+    dex = sum(d6.roll_n(3)) * 5
+    pow_ = sum(d6.roll_n(3)) * 5
+    app = sum(d6.roll_n(3)) * 5
+    luck = sum(d6.roll_n(3)) * 5
+    luck2 = sum(d6.roll_n(3)) * 5
+    siz = sum(d6.roll_n(2), 6) * 5
+    int_ = sum(d6.roll_n(2), 6) * 5
+    edu = sum(d6.roll_n(2), 6) * 5
+    mov = 8
+    if dex < siz and str_ < siz:
+        mov = 7
+    elif dex > siz and str_ > siz:
+        mov = 9
+
+    def edu_enhance(e: int):
+        if d100.roll() > e:
+            e += d10.roll()
+        return min(99, e)
+
+    warning = ""
+    if age < 15:
+        warning = "小于十五岁的调查员需要咨询KP调整属性值"
+    elif age < 20:
+        warning = "请将力量和体型合计减 5 点。"
+        luck = max(luck, luck2)
+    elif age < 40:
+        edu = edu_enhance(edu)
+    elif age < 50:
+        warning = "请将力量、敏捷和体质合计减 5 点。"
+        app -= 5
+        mov -= 1
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+    elif age < 60:
+        warning = "请将力量、敏捷和体质合计减 10 点。"
+        app -= 10
+        mov -= 2
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+    elif age < 70:
+        warning = "请将力量、敏捷和体质合计减 20 点。"
+        app -= 15
+        mov -= 3
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+    elif age < 80:
+        warning = "请将力量、敏捷和体质合计减 40 点。"
+        app -= 20
+        mov -= 4
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+    elif age <= 90:
+        warning = "请将力量、敏捷和体质合计减 80 点。"
+        app -= 25
+        mov -= 5
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+        edu = edu_enhance(edu)
+    else:
+        warning = "大于九十岁的调查员请询问KP"
+
+    stats = '''
+```
+力量  STR: {:2} 敏捷 DEX: {:2} 智力 INT: {:2}
+体质  CON: {:2} 外表 APP: {:2} 意志 POW: {:2}
+体形  SIZ: {:2} 教育 EDU: {:2} 移动 MOV: {:2}
+幸运 Luck: {:2}
+
+体力 HP: {:2} 理智 SAN: {:2} 魔法 MP: {:2}
+```
+
+已根据年龄调整了教育、移动力以及幸运。
+{}
+    '''.format(str_, dex, int_, con, app, pow_, siz, edu, mov, luck, (siz+con)//10, pow_, pow_//5, warning)
+    bot.send_message(chat_id, stats, parse_mode='Markdown')
 
 
 def set_default_dice(bot: Bot, update: Update, args: [str], chat_data: dict):
-    text = "诶呀，召唤我的方式出错了! `/set_default_dice` 后面跟一个 `d100` 或 `d20` 或 `d6` 哦"
+    chat_id = update.message.chat_id
     if len(args) != 1:
-        bot.send_message(update.message.chat_id, text, parse_mode='Markdown')
+        bot.send_message(
+            chat_id,
+            "诶呀，召唤我的方式出错了! `/set_default_dice` 后面跟一个形如 `d100` 的哦",
+            parse_mode='Markdown')
         return
 
-    arg = DICE_TYPE_PATTERN.match(args[0])
-    if arg is not None:
-        face_num = int(arg.group(1))
-        if face_num <= MAX_FACE:
-            chat_data['dice_type'] = 'normal'
-            chat_data['dice_face'] = face_num
-            bot.send_message(update.message.chat_id, "已设定默认骰子为{}面骰".format(face_num))
-            return
+    arg = args[0]
+    normal = DICE_TYPE_PATTERN.match(arg)
+    if normal is not None:
+        face_num = int(normal.group(1))
+        if face_num > MAX_FACE:
+            bot.send_message(chat_id, "骰子的面数太多了，你在想什么！")
+        chat_data['dice'] = Dice(face_num)
+        bot.send_message(chat_id, "已设定当前默认骰子为{}面骰".format(face_num))
+    elif arg == 'fate':
+        chat_data['dice'] = FateDice()
+        bot.send_message(chat_id, "已设定当前默认骰子为FATE骰子")
+    else:
+        bot.send_message(chat_id, "这种类型的骰子没办法设为默认骰子")
 
-    bot.send_message(update.message.chat_id, "暂时不支持这种类型的骰子设为默认骰子")
 
+def command_roll(bot: Bot, update: Update, args: [str], chat_data: dict):
+    msg = update.message
+    dice = Dice(face_num=100)
+    if 'dice' in chat_data:
+        dice = chat_data['dice']
 
+    dice_num = dice.DEFAULT_NUM
 
-def roll(bot: Bot, update: Update, args: [str], chat_data: dict):
-    dice_num = 1
-    dice_face = 100
-    if 'dice_type' in chat_data and chat_data['dice_type'] == 'normal':
-        dice_face = chat_data['dice_face']
     if len(args) == 1:
         roll_cmd = DICE_ROLL_PATTERN.match(args[0])
         if roll_cmd is not None:
             dice_num = int(roll_cmd.group(1))
-            dice_face = int(roll_cmd.group(2))
+            dice = Dice(int(roll_cmd.group(2)))
         else:
-            bot.send_message(update.message.chat_id, "抱歉，指令错误")
+            bot.send_message(msg.chat_id, "抱歉，指令错误")
             return
     if dice_num > MAX_NUM:
-        bot.send_message(update.message.chat_id, "找不到那么多骰子啦～～～")
+        bot.send_message(msg.chat_id, "找不到那么多骰子啦～")
         return
-    results = [dice(dice_face) for _ in range(dice_num)]
-    if dice_num == 1:
-        text = "`{}d{}` 🎲 `{}`".format(dice_num, dice_face, results[0])
-    else:
-        text = "`{}d{}` 🎲 `{}`\nsum: `{}` max: `{}` min: `{}`"\
-            .format(dice_num, dice_face, repr(results), sum(results), max(results), min(results))
-    bot.send_message(update.message.chat_id, text, reply_to_message_id=update.message.message_id, parse_mode='Markdown')
+    results = dice.roll_n(dice_num)
+
+    bot.send_message(msg.chat_id, dice.display(results),
+                     reply_to_message_id=msg.message_id, parse_mode='Markdown')
 
 
 def main():
     updater = Updater(token=BOT_TOKEN)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('r', roll, pass_args=True, pass_chat_data=True))
-    dispatcher.add_handler(CommandHandler('set_default_dice', set_default_dice, pass_args=True, pass_chat_data=True))
-    updater.start_polling()
+    dispatcher.add_handler(CommandHandler('r', command_roll, pass_args=True, pass_chat_data=True))
+    dispatcher.add_handler(CommandHandler('coc7stats', coc7stats, pass_args=True))
+    dispatcher.add_handler(
+        CommandHandler('set_default_dice', set_default_dice, pass_args=True, pass_chat_data=True))
+    if WEB_HOOK:
+        # TODO: work in processing.
+        updater.start_webhook(listen='127.0.0.1', port=5000, url_path=BOT_TOKEN)
+    else:
+        updater.start_polling()
     updater.idle()
 
 
